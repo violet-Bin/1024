3d04818be254a068b70f1cb7e7f34b92e8c434db
bingo <18845649698@163.com>
2019-01-09 01:33:38 +0800

    Label 1-13

---
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,25 @@
+package com.tensquare.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * @Author: jiangjiabin
+ * @Description:
+ * @Date: Create in 1:03 2019/1/9
+ */
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1,1);
+    }
+
+}
---
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/BaseController.java
@@ -0,0 +1,43 @@
+package com.tensquare.base.controller;
+
+import com.tensquare.base.pojo.Label;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Author: jiangjiabin
+ * @Description:
+ * @Date: Create in 1:13 2019/1/9
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/label")
+public class BaseController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll() {
+        return new Result(true, StatusCode.OK, "查询成功");
+    }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.GET)
+    public Result findById(@PathVariable("labelId") String labelId) {
+        return new Result(true, StatusCode.OK, "查询成功");
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Result save(@RequestBody Label label) {
+        return new Result(true, StatusCode.OK, "添加成功");
+    }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.PUT)
+    public Result update( @PathVariable String labelId, @RequestBody Label label) {
+        return new Result(true, StatusCode.OK, "更新成功");
+    }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String labelId) {
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+
+}
