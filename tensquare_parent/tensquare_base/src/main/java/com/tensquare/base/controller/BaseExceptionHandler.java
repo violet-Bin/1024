daa5dd56307822c5c894352b8af1798d20516767
bingo <18845649698@163.com>
2019-01-10 01:20:08 +0800

    first day end

---
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/BaseExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.tensquare.base.controller;
+
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Author: jiangjiabin
+ * @Description:
+ * @Date: Create in 1:04 2019/1/10
+ */
+@RestControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public Result exception(Exception e) {
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR, e.getMessage());
+    }
+
+}
