d2aad59a191f31b27949fae08afcc712cb1184cd
jiangjiabin3 <jiangjiabin3@jd.com>
2019-01-09 22:18:17 +0800

    database

--- tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/BaseController.java
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/BaseController.java
@@ -1,8 +1,10 @@
 package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
+import com.tensquare.base.service.LabelService;
 import entity.Result;
 import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -15,28 +17,35 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/label")
 public class BaseController {
 
+    @Autowired
+    private LabelService labelService;
+
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll() {
-        return new Result(true, StatusCode.OK, "查询成功");
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
     }
 
     @RequestMapping(value = "/{labelId}", method = RequestMethod.GET)
     public Result findById(@PathVariable("labelId") String labelId) {
-        return new Result(true, StatusCode.OK, "查询成功");
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findById(labelId));
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public Result save(@RequestBody Label label) {
+        labelService.save(label);
         return new Result(true, StatusCode.OK, "添加成功");
     }
 
     @RequestMapping(value = "/{labelId}", method = RequestMethod.PUT)
     public Result update( @PathVariable String labelId, @RequestBody Label label) {
+        label.setId(labelId);
+        labelService.update(label);
         return new Result(true, StatusCode.OK, "更新成功");
     }
 
     @RequestMapping(value = "/{labelId}", method = RequestMethod.DELETE)
     public Result deleteById(@PathVariable String labelId) {
+        labelService.deleteById(labelId);
         return new Result(true, StatusCode.OK, "删除成功");
     }
 
