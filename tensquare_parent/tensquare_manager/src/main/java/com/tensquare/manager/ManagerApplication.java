380f0b0691571b9b95a0e2bef5243b524b667070
bingo <18845649698@163.com>
2019-01-20 20:17:08 +0800

    configfile

--- tensquare_parent/tensquare_manager/src/main/java/com/tensquare/manager/ManagerApplication.java
+++ tensquare_parent/tensquare_manager/src/main/java/com/tensquare/manager/ManagerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import util.JwtUtil;
 
 /**
  * @Author: jiangjiabin
@@ -17,6 +19,11 @@ public class ManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class);
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil();
     }
 
 }
--- tensquare_parent/tensquare_manager/src/main/java/com/tensquare/manager/filter/ManagerFilter.java
+++ tensquare_parent/tensquare_manager/src/main/java/com/tensquare/manager/filter/ManagerFilter.java
@@ -1,8 +1,16 @@
 package com.tensquare.manager.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import io.jsonwebtoken.Claims;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import util.JwtUtil;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author: jiangjiabin
@@ -12,8 +20,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class ManagerFilter extends ZuulFilter {
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     /**
      * 在请求前pre或者后post执行
+     *
      * @return
      */
     @Override
@@ -23,6 +35,7 @@ public class ManagerFilter extends ZuulFilter {
 
     /**
      * 多个过滤器的执行顺序
+     *
      * @return
      */
     @Override
@@ -32,6 +45,7 @@ public class ManagerFilter extends ZuulFilter {
 
     /**
      * 当前过滤器是否开启
+     *
      * @return
      */
     @Override
@@ -42,12 +56,46 @@ public class ManagerFilter extends ZuulFilter {
     /**
      * 过滤器里执行的操作  return 任何object的值都表示继续执行
      * setsendzullRepose(false)表示不再继续执行
+     *
      * @return
      * @throws ZuulException
      */
     @Override
     public Object run() throws ZuulException {
         System.out.println("经过后台过滤器了");
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+
+        if (request.getMethod().equals("OPTIONS")) {
+            return null;
+        }
+
+        if (request.getRequestURI().indexOf("login") > 0) {
+            return null;
+        }
+
+
+        String header = request.getHeader("Authorization");
+        if (!StringUtils.isEmpty(header)) {
+            if (header.startsWith("Bearer ")) {
+                String token = header.substring(7);
+                try {
+                    Claims claims = jwtUtil.parseJWT(token);
+                    String roles = (String) claims.get("roles");
+                    if (StringUtils.equals(roles, "admin")) {
+                        currentContext.addZuulRequestHeader("Authorization", header);
+                        return null;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    currentContext.setSendZuulResponse(false);
+                }
+            }
+        }
+        currentContext.setSendZuulResponse(false);
+        currentContext.setResponseStatusCode(403);
+        currentContext.setResponseBody("权限不足！");
+        currentContext.getResponse().setContentType("text/html;charset=utf-8");
         return null;
     }
 }
